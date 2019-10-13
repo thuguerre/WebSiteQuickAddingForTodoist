3e8f93d4cddcea52da2091515a1431fd318299ed
thuguerre <huguerre.thomas@gmail.com>
2019-10-13 13:50:07 +0200

    improvment of one test

--- TodoistProxyAPI/src/test/java/com/thug/CredentialsTest.java
+++ TodoistProxyAPI/src/test/java/com/thug/CredentialsTest.java
@@ -13,10 +13,7 @@ public class CredentialsTest {
 	@Test
 	public void isCredentialsPropertiesFilePresent() throws IOException {
 
-		InputStream input = getClass().getResourceAsStream("/credentials.properties");
-
-		Properties prop = new Properties();
-		prop.load(input);
+		Assert.assertNotNull("Credentials.properties file does not exist.", getClass().getResourceAsStream("/credentials.properties"));
 	}
 
 	@Test
@@ -31,7 +28,7 @@ public class CredentialsTest {
 		String clientSecret = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_SECRET_PROPERTY_ID);
 
 		Pattern credentialsPattern = Pattern.compile("^[0-9a-f]{32}$");
-		
+
 		Assert.assertFalse("Todoist Client ID property is not found", clientId == null);
 		Assert.assertFalse("Todoist Client ID property is not found", clientId.length() == 0);
 		Assert.assertTrue("Client ID does not match pattern", credentialsPattern.matcher(clientId).matches());
