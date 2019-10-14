6c00d485b62816be0a5a34cd29ed1e66c8c51261
thuguerre <huguerre.thomas@gmail.com>
2019-10-14 13:11:44 +0200

    A small commit and test to test GitHub Secrets function

--- TodoistProxyAPI/src/test/java/com/thug/CredentialsTest.java
+++ TodoistProxyAPI/src/test/java/com/thug/CredentialsTest.java
@@ -10,31 +10,53 @@ import java.util.regex.Pattern;
 
 public class CredentialsTest {
 
-	@Test
-	public void isCredentialsPropertiesFilePresent() throws IOException {
+    @Test
+    public void isCredentialsPropertiesFilePresent() throws IOException {
 
-		Assert.assertNotNull("Credentials.properties file does not exist.", getClass().getResourceAsStream("/credentials.properties"));
-	}
+        Assert.assertNotNull("Credentials.properties file does not exist.", getClass().getResourceAsStream("/credentials.properties"));
+    }
 
-	@Test
-	public void areCredentialsPropertiesPresent() throws IOException {
+    @Test
+    public void areCredentialsPropertiesPresent() throws IOException {
 
-		InputStream input = getClass().getResourceAsStream("/credentials.properties");
+        InputStream input = getClass().getResourceAsStream("/credentials.properties");
 
-		Properties prop = new Properties();
-		prop.load(input);
+        Properties prop = new Properties();
+        prop.load(input);
 
-		String clientId = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_ID_PROPERTY_ID);
-		String clientSecret = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_SECRET_PROPERTY_ID);
+        String clientId = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_ID_PROPERTY_ID);
+        String clientSecret = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_SECRET_PROPERTY_ID);
 
-		Pattern credentialsPattern = Pattern.compile("^[0-9a-f]{32}$");
+        Pattern credentialsPattern = Pattern.compile("^[0-9a-f]{32}$");
 
-		Assert.assertFalse("Todoist Client ID property is not found", clientId == null);
-		Assert.assertFalse("Todoist Client ID property is not found", clientId.length() == 0);
-		Assert.assertTrue("Client ID does not match pattern", credentialsPattern.matcher(clientId).matches());
+        Assert.assertFalse("Todoist Client ID property is not found", clientId == null);
+        Assert.assertFalse("Todoist Client ID property is not found", clientId.length() == 0);
+        Assert.assertTrue("Client ID does not match pattern", credentialsPattern.matcher(clientId).matches());
 
-		Assert.assertFalse("Todoist Client Secret property is not found", clientSecret == null);
-		Assert.assertFalse("Todoist Client Secret property is not found", clientSecret.length() == 0);
-		Assert.assertTrue("Client Secret does not match pattern", credentialsPattern.matcher(clientSecret).matches());
-	}
+        Assert.assertFalse("Todoist Client Secret property is not found", clientSecret == null);
+        Assert.assertFalse("Todoist Client Secret property is not found", clientSecret.length() == 0);
+        Assert.assertTrue("Client Secret does not match pattern", credentialsPattern.matcher(clientSecret).matches());
+    }
+
+    @Test
+    public void areCredentialsTestPropertiesPresent() throws IOException {
+
+        InputStream input = getClass().getResourceAsStream("/credentials_test.properties");
+
+        Properties prop = new Properties();
+        prop.load(input);
+
+        String clientId = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_ID_PROPERTY_ID);
+        String clientSecret = prop.getProperty(TodoistProxyAPI.TODOIST_CLIENT_SECRET_PROPERTY_ID);
+
+        Pattern credentialsPattern = Pattern.compile("^[0-9a-f]{32}$");
+
+        Assert.assertFalse("Todoist Client ID property is not found", clientId == null);
+        Assert.assertFalse("Todoist Client ID property is not found", clientId.length() == 0);
+        Assert.assertTrue("Client ID does not match pattern", credentialsPattern.matcher(clientId).matches());
+
+        Assert.assertFalse("Todoist Client Secret property is not found", clientSecret == null);
+        Assert.assertFalse("Todoist Client Secret property is not found", clientSecret.length() == 0);
+        Assert.assertTrue("Client Secret does not match pattern", credentialsPattern.matcher(clientSecret).matches());
+    }
 }
