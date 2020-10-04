45e9c42ccc64564d979ef3097174f52f2d5141fe
Thomas Huguerre <huguerre.thomas@gmail.com>
2020-10-04 09:21:13 +0200

    add remote test
    ... in order to verify the cloud function is well deployed and functional.
    this test works from local execution. Let's see if the remote execution from github actions works too.

--- TodoistProxyAPIPython/main_test.py
+++ TodoistProxyAPIPython/main_test.py
@@ -1,5 +1,7 @@
 from main import simple_cloud_function
 import unittest
+import requests
+import os, uuid
 from unittest.mock import Mock
 
 # https://cloud.google.com/functions/docs/testing/test-http?hl=fr
@@ -12,10 +14,22 @@ class TestMain(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_simple_cloud_function(self):
+    def test_local_simple_cloud_function(self):
         data = {}
         req = Mock(get_json=Mock(return_value=data), args=data)
 
         # Call tested function
         assert simple_cloud_function(req) == '''<h1>TEST</h1>'''
+
+    def test_remote_simple_cloud_function(self):
+        # BASE_URL = os.getenv('BASE_URL')
+        BASE_URL = 'https://europe-west1-websitequickadding4todoisttest.cloudfunctions.net/test-to-delete'
+        assert BASE_URL is not None
+
+        name = str(uuid.uuid4())
+        res = requests.post(
+            '{}/hello_http'.format(BASE_URL),
+            json={'name': name}
+        )
+        assert res.text == '''<h1>TEST</h1>'''
 
