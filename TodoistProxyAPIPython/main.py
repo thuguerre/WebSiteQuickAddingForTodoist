63b36e5961486d176aaefdb5d72508328834b136
Thomas Huguerre <huguerre.thomas@gmail.com>
2020-10-04 14:20:34 +0200

    rename auto test to auto deply python

--- TodoistProxyAPIPython/main.py
+++ TodoistProxyAPIPython/main.py
@@ -1,4 +1,2 @@
-# gcloud functions deploy test-to-delete --entry-point simple_cloud_function --region europe-west1 --runtime python38 --trigger-http --allow-unauthenticated
-
 def simple_cloud_function(request):
     return '''<h1>TEST1</h1>'''
