4a8c05d2b0b192c99d6f705b393d90002ad96f97
Thomas Huguerre <huguerre.thomas@gmail.com>
2020-10-04 09:00:18 +0200

    init cloud function
    transforming flash app into more cloud function. for the moment, the cloud function has nothing functional for the project but has been successfully deployed to GCP and correctly returns TEST

---
+++ TodoistProxyAPIPython/main_test.py
@@ -0,0 +1,23 @@
+from main import simple_cloud_function
+import unittest
+from unittest.mock import Mock
+
+# https://cloud.google.com/functions/docs/testing/test-http?hl=fr
+
+class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+    def test_simple_cloud_function(self):
+        name = 'test'
+        data = {'name': name}
+        req = Mock(get_json=Mock(return_value=data), args=data)
+
+        # Call tested function
+        assert simple_cloud_function(req) == '''<h1>TEST</h1>'''
+
